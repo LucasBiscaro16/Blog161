6fd7e0ac735cb17426b0bfd5e99eb5cbbe4c19d3
LUCAS BISCARO DE SENA <lucas_biscaro@hotmail.com>
2019-09-13 14:36:23 -0400

    Trabalho

---
+++ Blog1661/Blog1661/Controllers/CrllrComent.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CrllrComent : Controller
+    {
+        private readonly WaContext _context;
+
+        public CrllrComent(WaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Comentario.ToListAsync());
+        }
+
+       
+
+    
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comentario = await _context.Comentario.FindAsync(id);
+            _context.Comentario.Remove(comentario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentarioExists(int id)
+        {
+            return _context.Comentario.Any(e => e.ComentarioId == id);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ComentarioId,Titulo,Descricao,DataDoComentario,Autor")] Comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comentario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ComentarioId,Titulo,Descricao,DataDoComentario,Autor")] Comentario comentario)
+        {
+            if (id != comentario.ComentarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentarioExists(comentario.ComentarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(comentario);
+
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentario.FindAsync(id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+            return View(comentario);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comentario = await _context.Comentario
+                .FirstOrDefaultAsync(m => m.ComentarioId == id);
+            if (comentario == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentario);
+        }
+       
+
+
+
+        
+
+ 
+      
+    }
+}
---
+++ Blog1661/Blog1661/Db/WaContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Models
+{
+    public class WaContext : DbContext
+    {
+        public WaContext (DbContextOptions<WaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public DbSet<Mensagem> Mensagem { get; set; }
+
+    }
+}
